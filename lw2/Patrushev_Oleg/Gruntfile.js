20d7c911483380eeb1e86bba05154a289d2099cd
Oleg <patrushev1996@gmail.com>
2017-03-29 02:22:47 +0300

    Finished (maybe)

--- lw2/Patrushev_Oleg/Gruntfile.js
+++ lw2/Patrushev_Oleg/Gruntfile.js
@@ -1,12 +1,41 @@
 module.exports = function( grunt ) {
   grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+		cachebreaker: {
+			dev: {
+				options: {
+					match: [{
+						'scripts.js': 'build/scripts.js',
+						'styles.css': 'build/styles.css'
+					}],
+					replacement: 'md5',
+				},
+				files: {
+					src: ['build/index.html']
+				}
+			}
+		},
+		assets_hash: {
+			assets: {
+				options: {
+					algorithm: 'md5',
+					jsonFile: 'versions.json',
+					length: 10,
+					clear: false,
+					suffix: true
+				},
+				src: ['build/scripts.js', 'build/styles.css']
+			}
+		},
 		eslint: {
-			target: ['qwer.js']
+			options: {
+				configFile: "eslint.json",
+			},
+			src: ['project/js/*.js']
 		},
         concat: {
             css: {
-                src: ['project/styles/*.css'],
+                src: ['project/styles/*.css', 'node_modules/bootstrap/dist/css/bootstrap.min.css', 'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'],
                 dest: 'build/styles.css'
             },
             js: {
@@ -21,41 +50,40 @@ module.exports = function( grunt ) {
 		connect: {
 			server: {
 				options: {
+					hostname: 'localhost',
 					port: 8080,
 					livereload: true,
-					hostname: 'localhost'
-				},
-				livereload: {
-					options: {
-						open: true,
-						base: ['build/']
-					}
+					base: 'build/'
 				}
 			}
 		},
         watch: {
-
+			options: {
+				livereload: true
+			},
             css: {
                 files: ['project/styles/*.css'],
-                tasks: ['concat:css', 'default']
+                tasks: ['concat:css', 'assets_hash', 'cachebreaker']
             },
             js: {
                 files: ['project/js/*.js'],
-                tasks: ['concat:js', 'default']
+                tasks: ['eslint', 'concat:js', 'assets_hash', 'cachebreaker']
             },
 			html: {
 				files: ['project/index.html'],
-                tasks: ['default']
+                tasks: ['assets_hash', 'cachebreaker']
 			}
         }
     });
 
     // 2. Where we tell Grunt we plan to use this plug-in.
-	grunt.loadNpmTasks('grunt-contrib-eslint');
+	grunt.loadNpmTasks("gruntify-eslint");
     grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-assets-hash');
+	grunt.loadNpmTasks('grunt-cache-breaker');
 
     // 3. Where we tell Grunt what to do when we type "grunt" into the terminal.
-    grunt.registerTask('default', ['eslint', 'concat', 'connect', 'watch']);
+    grunt.registerTask('default', ['eslint', 'concat', 'assets_hash', 'cachebreaker', 'connect', 'watch']);
 };
