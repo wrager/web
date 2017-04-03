247f309e295427dc71a49004a130e7367dc449b1
Bochkarev Maxim <qontrol1488@gmail.com>
2017-04-04 00:57:31 +0300

    Added concat task - files are now concated and moved to /.build folder

--- lw2/Bochkarev_Maxim/Gruntfile.js
+++ lw2/Bochkarev_Maxim/Gruntfile.js
@@ -7,11 +7,25 @@ module.exports = function(grunt){
 			options: {
 				configFile: "eslint_config.json",
 			}		
-},
-		
+		},
+		concat: {
+			js: {
+				src: ['src/Scripts/*js'],
+				dest: '.build/scripts.js'
+			},
+			html: {
+				src: ['src/site.html'],
+				dest: '.build/index.html'
+			},
+			css: {
+				src: ['node_modules/bootstrap/dist/css/bootstrap.min.css', 'src/styles/style.css'],
+				dest: '.build/styles.css'
+			}
+		}
 	});
 	grunt.loadNpmTasks('grunt-eslint');	
 	grunt.loadNpmTasks('grunt-contrib-connect');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default', ['eslint']);
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.registerTask('default', ['eslint', 'concat']);
 };
