7bad02f986a8b007fb4ce93dffa8ed368b396ba0
Oleg <patrushev1996@gmail.com>
2017-04-07 22:43:54 +0300

    One method set to private(getLineLength), can not do the same with other, because it is used outside the constructor, when adding other methods inside constructor it won't work correct

--- lw2/Patrushev_Oleg/project/js/triangle.js
+++ lw2/Patrushev_Oleg/project/js/triangle.js
@@ -13,12 +13,24 @@ function Triangle() {
     }
 
      this.getSides = function() {
-        var sides = [];
-        sides.push(getLineLength(this.getP1X(), this.getP1Y(), this.getP2X(), this.getP2Y()));
-        sides.push(getLineLength(this.getP1X(), this.getP1Y(), this.getP3X(), this.getP3Y()));
-        sides.push(getLineLength(this.getP2X(), this.getP2Y(), this.getP3X(), this.getP3Y()));
+        var line1 = getLineLength(this.getP1X(), this.getP1Y(), this.getP2X(), this.getP2Y());
+        var line2 = getLineLength(this.getP1X(), this.getP1Y(), this.getP3X(), this.getP3Y());
+        var line3 = getLineLength(this.getP2X(), this.getP2Y(), this.getP3X(), this.getP3Y());
 
-        return sides;
+        return [line1, line2, line3];
+    }
+
+    this.calculateArea = function () {
+        var p = this.calculatePerimeter() / 2;
+        var lines = this.getSides(); 
+
+        return (Math.sqrt(p * (p - lines[0]) * (p - lines[1]) * (p - lines[2]))).toFixed(3);
+    }
+
+    this.calculatePerimeter = function () {
+        var lines = this.getSides();
+
+        return (lines[0] + lines[1] + lines[2]).toFixed(3);
     }
 }
 
@@ -83,17 +95,4 @@ Triangle.prototype.draw = function (context) {
     context.fill();
     context.strokeStyle = this.getBorderColor();
     context.stroke();
-}
-
-Triangle.prototype.calculateArea = function () {
-    var p = this.calculatePerimeter() / 2;
-    var lines = this.getSides(); 
-
-    return (Math.sqrt(p * (p - lines[0]) * (p - lines[1]) * (p - lines[2]))).toFixed(3);
-}
-
-Triangle.prototype.calculatePerimeter = function () {
-    var lines = this.getSides();
-
-    return (lines[0] + lines[1] + lines[2]).toFixed(3);
 }
