315efcdef179617bfdf524b70bf1fe8222080392
AniSkyWorker <holyrecycler@mail.ru>
2017-04-24 13:54:38 +0300

    добавил файл для сборки grunt, перенес файлы в подпапку src

---
+++ lw2/Anikin_Oleg/gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function( grunt ) {
+  grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            css: {
+                src: ['src/styles/*.css', 'node_modules/bootstrap/dist/css/bootstrap.min.css', 'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'],
+                dest: 'build/styles.css'
+            },
+            js: {
+                src: ['src/js/*.js'],
+                dest: 'build/scripts.js'
+            },
+			html: {
+				src: ['src/index.html'],
+                dest: 'build/index.html'
+			}
+        },
+		connect: {
+			server: {
+				options: {
+					hostname: 'localhost',
+					port: 8080,
+					base: 'build/'
+				}
+			}
+		},
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.registerTask('default', ['concat', 'connect']);
+};
