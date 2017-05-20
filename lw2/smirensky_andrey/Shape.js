0215a58cde3d6daaef5c405917935bd8a4824df7
ASmMegane <a_n_d_i@bk.ru>
2017-05-20 13:50:59 +0300

    shape

---
+++ lw2/smirensky_andrey/Shape.js
@@ -0,0 +1,34 @@
+function Shape() {
+    this.fillColor = '#000000';
+    this.borderColor = '#000000';
+}
+
+Shape.prototype = {
+    getFillColor: function () {
+        return this.fillColor;
+    },
+
+    setFillColor: function (color) {
+        this.fillColor = color;
+    },
+
+    getBorderColor: function () {
+        return this.borderColor;
+    },
+
+    setBorderColor: function (color) {
+        this.borderColor = color;
+    },
+
+    calculateArea: function () {
+
+    },
+
+    calculatePerimeter: function () {
+
+    },
+
+    draw: function () {
+
+    }
+};
