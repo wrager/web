0af8385edca586ceafa4d110e632daa52c951ba3
ultimatum424 <Good20032014>
2017-06-01 22:36:51 +0300

    add js Files

---
+++ lw2/Veschev_Alexey/JS/circle.js
@@ -0,0 +1,57 @@
+function Cirlce(){
+    Shape.apply(this, arg);
+    this.mRadius = 0;
+    this.mX = 0;
+    this.mY = 0;
+}
+Circle.prototype = Object.create(Shape.prototype);
+Circle.prototype.constructor = Circle
+
+//Set param
+Circle.prototype.setRadius = function (value) {
+    this.mRadius = value;
+}
+
+Circle.prototype.setRadius = function (x, y) {
+    this.mX = x;
+    this.mY = y;
+}
+
+Circle.prototype.setX = function (x) {
+    this.mX = x;
+}
+
+Circle.prototype.setY = function (y) {
+    this.mY = y;
+}
+
+Circle.prototype.getRadius = function () {
+    return this.mRadius;
+}
+
+Circle.prototype.getX = function (x) {
+    return this.mX;
+}
+
+Circle.prototype.getY = function (y) {
+    return this.mY;
+}
+
+//Draw
+Circle.prototype.draw = function (context) {
+    context.beginPath();
+    context.arc(this.getX(), this.getX(), this.getRadius(), 0, 2 * Math.PI, false);
+    context.fillStyle = this.getFillColor();
+    context.fill();
+    context.strokeStyle = this.getBorderColor();
+    context.stroke();
+}
+
+//calculate
+Circle.prototype.calculateArea = function () {
+    return (Math.PI * Math.pow(this.mRadius, 2)).toFixed(3);
+}
+
+Circle.prototype.calculatePerimeter = function () {
+    return (2 * Math.PI * this.r).toFixed(3);
+}
