968c3af37e569a4a31906635b6a99c93f2ecf705
Biryukov Georgy <intmpg@gmail.com>
2017-06-02 11:27:48 +0300

    may be it can fix refreshing bug

--- lw2/Birykov/js/script.js
+++ lw2/Birykov/js/script.js
@@ -22,72 +22,57 @@ function onShapeSelect() {
     }
 }
 
-function onApplyClick() {
-	
-    var canvas = getElement("canvas");
-    var context = canvas.getContext("2d");
-    var selectedValue = getSelectorValue();
-	
+function clearCanvas() {
+    const context = canvas.getContext("2d") as CanvasRenderingContext2D;
     context.clearRect(0, 0, canvas.width, canvas.height);
+}
 
-    if (selectedValue == "Circle") {
+function clickOnDraw() {
+
+    const selectedValue = getSelectorValue();
+    const context = canvas.getContext("2d") as CanvasRenderingContext2D;
+    clearCanvas();
+
+    if (selectedValue === "Circle") {
         circle = new Circle();
-		
         circle.setFillColor(getElementValue("input-fill-color"));
         circle.setBorderColor(getElementValue("input-border-color"));
-        circle.setRadius(getElementValue("circle-r"));
-        circle.setX(getElementValue("circle-x"));
-        circle.setY(getElementValue("circle-y"));
-    } else if (selectedValue == "Rectangle") {
+        circle.setRadius(parseInt(getElementValue("input-circle-r"), 10));
+        circle.setX(parseInt(getElementValue("input-circle-x"), 10));
+        circle.setY(parseInt(getElementValue("input-circle-y"), 10));
+        circle.draw(context);
+        drawShapeInfo(context, circle);
+    } else if (selectedValue === "Rectangle") {
         rectangle = new Rectangle();
-		
         rectangle.setFillColor(getElementValue("input-fill-color"));
         rectangle.setBorderColor(getElementValue("input-border-color"));
-        rectangle.setX1(getElementValue("rectangle-x1"));
-        rectangle.setY1(getElementValue("rectangle-y1"));
-        rectangle.setX2(getElementValue("rectangle-x2"));
-        rectangle.setY2(getElementValue("rectangle-y2"));
-    } else if (selectedValue == "Triangle") {
+        rectangle.setX1(parseInt(getElementValue("input-rectangle-x1"), 10));
+        rectangle.setY1(parseInt(getElementValue("input-rectangle-y1"), 10));
+        rectangle.setX2(parseInt(getElementValue("input-rectangle-x2"), 10));
+        rectangle.setY2(parseInt(getElementValue("input-rectangle-y2"), 10));
+        rectangle.draw(context);
+        drawShapeInfo(context, rectangle);
+    } else if (selectedValue === "Triangle") {
         triangle = new Triangle();
-		
         triangle.setFillColor(getElementValue("input-fill-color"));
         triangle.setBorderColor(getElementValue("input-border-color"));
-        triangle.setP1X(getElementValue("triangle-x1"));
-        triangle.setP1Y(getElementValue("triangle-y1"));
-        triangle.setP2X(getElementValue("triangle-x2"));
-        triangle.setP2Y(getElementValue("triangle-y2"));
-        triangle.setP3X(getElementValue("triangle-x3"));
-        triangle.setP3Y(getElementValue("triangle-y3"));
+        triangle.setP1X(parseInt(getElementValue("input-triangle-x1"), 10));
+        triangle.setP1Y(parseInt(getElementValue("input-triangle-y1"), 10));
+        triangle.setP2X(parseInt(getElementValue("input-triangle-x2"), 10));
+        triangle.setP2Y(parseInt(getElementValue("input-triangle-y2"), 10));
+        triangle.setP3X(parseInt(getElementValue("input-triangle-x3"), 10));
+        triangle.setP3Y(parseInt(getElementValue("input-triangle-y3"), 10));
+        triangle.draw(context);
+        drawShapeInfo(context, triangle);
     }
-
-    draw([circle, rectangle, triangle], context);
 }
 
-function exist(item) {
-	
-    return typeof (item) != 'undefined' && item != null;
-}
-
-function draw(shapes, context) {
-	
-    var index = 1;
-
-    shapes.forEach(function(element) {
-        if (exist(element)) {
-            element.draw(context);
-            drawShapeInfo(context, element, index);
-            index+=2;
-        }
-    });
-}
-
-function drawShapeInfo(context, shape, shift) {
-	
-    context.font=FONT_SIZE + "px Montserrat Alternates";
+function drawShapeInfo(context: CanvasRenderingContext2D, shape: Shape) {
+    context.font = FONT_SIZE + "px Montserrat Alternates";
     context.fillStyle = INFO_TEXT_COLOR;
-    context.fillText(shape.constructor.name, 300, shift * FONT_SIZE * 2);
-    context.fillText("Perimeter: " + shape.calculatePerimeter(), 300, shift * FONT_SIZE * 2 + FONT_SIZE);
-    context.fillText("Area: " + shape.calculateArea(), 300, shift * FONT_SIZE * 2 + FONT_SIZE * 2);
+    context.fillText(shape.constructor.toString().match(/\w+/g)[1], 800, shift * FONT_SIZE * 2);
+    context.fillText("Perimeter: " + shape.calculatePerimeter(), 800, shift * FONT_SIZE * 2 + FONT_SIZE);
+    context.fillText("Area: " + shape.calculateArea(), 800, shift * FONT_SIZE * 2 + FONT_SIZE * 2);
 }
 
 function getElement(id) {
